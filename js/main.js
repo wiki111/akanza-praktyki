b6a7211de2f68c5b1213b7cc925b0e8cb11d6d6d
Maciej Daniel Wikira <maciej.wiqira@gmail.com>
2017-07-03 08:55:16 +0000

    Finished pixel perfect layout and added responsiveness

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
