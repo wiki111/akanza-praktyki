69c092cf6a33c7fc21de5fae54ac55052a6122a9
Maciej Daniel Wikira <maciej.wiqira@gmail.com>
2017-07-03 12:07:29 +0000

    Added change of active menu item when page scrolls to relevant section and changed collapsed menu icon

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ function showMenu() {
     }
 }
 $(document).ready(function(){
+    $(document).on("scroll", onScroll);
+    
     $('#logo, #start').click(
         function(event){
             $('html,body').animate({scrollTop: 0}, 'fast');
@@ -15,8 +17,27 @@ $(document).ready(function(){
             {
                 x.className = "menu"; 
             }
-            return false;});
+            return false;
+        }
+    );
 });
+
+function onScroll(event){
+    var currentPosition = $(document).scrollTop();
+    $('#menu ul li a').each(function() {
+       var currentLink = $(this);
+       var referenceElement = $(currentLink.attr("href"));
+       var top = referenceElement.position().top - 200;
+       if ( top <= currentPosition) {
+            $('#menu ul li a').removeClass("red");
+            currentLink.addClass("red");
+        }
+        else{
+            currentLink.removeClass("red");
+        }
+    });
+}
+
 function scrollToDiv(div){	
     var offset = $('#'+div).offset().top;
     var offset = offset - 80;	
@@ -24,5 +45,5 @@ function scrollToDiv(div){
     var x = document.getElementById("menu");
     if(x.className === "menu responsive"){
         x.className = "menu";
-    }  
+    } 
 }
