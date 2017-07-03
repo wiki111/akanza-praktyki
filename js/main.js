1cf2b9ef21428f9a136021d0a4772d797e0155de
Maciej Daniel Wikira <maciej.wiqira@gmail.com>
2017-07-03 11:07:08 +0000

    Cleaned up code, added custom 404 page, and minor tweaks to responsive layout

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ function showMenu() {
         x.className = "menu";
     }
 }
+$(document).ready(function(){
+    $('#logo, #start').click(
+        function(event){
+            $('html,body').animate({scrollTop: 0}, 'fast');
+            var x = document.getElementById("menu");
+            if(x.className === "menu responsive")
+            {
+                x.className = "menu"; 
+            }
+            return false;});
+});
+function scrollToDiv(div){	
+    var offset = $('#'+div).offset().top;
+    var offset = offset - 80;	
+    $('html,body').animate({scrollTop: offset});	
+    var x = document.getElementById("menu");
+    if(x.className === "menu responsive"){
+        x.className = "menu";
+    }  
+}
